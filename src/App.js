49c43a46265231d8cb4081725d5b8f7245bccffd
mikescode199 <mikesservices199@gmail.com>
2020-12-11 19:28:37 -0800

    1.0

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import React, { useState } from 'react';
 
 function App() {
   const[ inputText, setInputText] = useState("");
+  const[todos, setTodos] = useState([]);
   
   return (
     <div className="App">
       <header>
-      <h1>Mike's Todo </h1>
+      <h1>Mike's Todo { setInputText } </h1>
       </header>
 
-      <Form />
-      <TodoList />
+      <Form 
+        inputText = {inputText}
+        todos = {todos}
+        setTodos={setTodos} 
+        setInputText={setInputText}
+      />
+      <TodoList todos={todos} />
 
     </div>
   );
